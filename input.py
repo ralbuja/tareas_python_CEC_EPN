3cbeb15906da3fdcfd8c8fac7d8835d26d6f0cc0
Rommel Albuja <ralbuja@users.noreply.github.com>
2023-08-07 18:05:37 -0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  7 17:42:29 2023
+
+@author: Rommel Albuja
+"""
+
+# =============================================================================
+# print ("Programa INPUT")
+# =============================================================================
+
+print ("Programa de ingreso de información de una persona")
+nombre = input("Ingrese el nombre:")
+apellido = input ("Ingrese el apellido:")
+ubicacion = input("Ingrese la ubicación:")
+edad = input ("Ingrese la edad de la persona:")
+
+# Se muestra el párrafo de ejemplo.
+parrafo = f"La persona registrada responde al siguiente nombre: {nombre} {apellido}. Vive en {ubicacion} y tiene {edad} años."
+
+print (parrafo)
+
